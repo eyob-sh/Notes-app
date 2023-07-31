bc5f70d57ab82ec014f765e0f6366c1152c5279e
Eyob Shewangizaw <eyobsh72@gmail.com>
2023-07-31 04:17:01 -0500

    fetch notes from backend and display on front page

--- frontend/src/Note.jsx
+++ frontend/src/Note.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./notebox.css";
 
-function Note() {
+function Note(props) {
   return (
     <div className="note">
       <div className="top">
         <span style={{ color: "blue" }}>update</span>
         <span style={{ color: "red" }}>delete</span>
+      </div>
+      <div className="displayedNote">
+        <h4 className="noteTitle">{props.title}</h4>
+        <p className="noteContent">{props.note}</p>
       </div>
     </div>
   );
--- frontend/src/frontpage.jsx
+++ frontend/src/frontpage.jsx
@@ -10,23 +10,33 @@ import { Link } from "react-router-dom";
 
 function FrontPage() {
   const [visible, setVisible] = React.useState(false);
+  const [notes, setNotes] = React.useState([]);
 
   function handleClick() {
     setVisible((prev) => !prev);
   }
+  React.useEffect(() => {
+    fetch("http://127.0.0.1:8000/note/get/")
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+  }, []);
+
+  console.log(notes);
+  const displayNote = notes.map(function (item) {
+    return <Note title={item.title} note={item.content} />;
+  });
+  console.log(displayNote);
   return (
     <div>
       <div id="header">
-        <h1 className="title" title="add note">
-          Notes
-        </h1>
+        <h1 className="title">Notes</h1>
 
-        <button className="button" onClick={handleClick}>
+        <button title="add note" className="button" onClick={handleClick}>
           +
         </button>
       </div>
       <main className="main">
-        <Note />
+        {displayNote}
         <Form visible={visible} onClick={handleClick} />
       </main>
     </div>
